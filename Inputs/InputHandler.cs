6db5b747c65615d940cd4c81a3a81b57d9490729
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-02-23 20:01:19 -0500

    Movement, Pause, Camera Control
    pushing the main work done for character controlling, pause implementation, and camera control.

---
+++ Inputs/InputHandler.cs
@@ -0,0 +1,100 @@
+//Purpose: To set the ground work for the input system and the Action Map
+//Contributor and Author: Logan Baysinger
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset playerControls;
+
+    [SerializeField] private string actionMapName = "Player";
+
+    [SerializeField] private string move = "Move";
+    [SerializeField] private string sprint = "Sprint";
+    [SerializeField] private string pause = "Pause";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string fire = "Fire";
+    [SerializeField] private string altFire = "AltFire";
+
+    private InputAction moveAction;
+    private InputAction sprintAction;
+    private InputAction pauseAction;
+    private InputAction jumpAction;
+    private InputAction fireAction;
+    private InputAction altFireAction;
+
+    public Vector2 MoveInput { get; private set; }
+    public float SprintValue { get; private set; }
+    public bool PauseInput {  get; set; }
+    public bool JumpInput { get; set; }
+    public bool FireInput { get; private set; }
+    public bool altFireInput { get; private set; }
+
+    public static InputHandler Instance { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Destroy(gameObject);
+        }
+
+        moveAction = playerControls.FindActionMap(actionMapName).FindAction(move);
+        sprintAction = playerControls.FindActionMap(actionMapName).FindAction(sprint);
+        pauseAction = playerControls.FindActionMap(actionMapName).FindAction(pause);
+        jumpAction = playerControls.FindActionMap(actionMapName).FindAction(jump);
+        fireAction = playerControls.FindActionMap(actionMapName).FindAction(fire);
+        altFireAction = playerControls.FindActionMap(actionMapName).FindAction(altFire);
+        RegisterInputActions();
+    }
+
+    void RegisterInputActions()
+    {
+        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
+        moveAction.canceled += context => MoveInput = Vector2.zero;
+
+        sprintAction.performed += context => SprintValue = context.ReadValue<float>();
+        sprintAction.canceled += context => SprintValue = 0f;
+
+        pauseAction.performed += context => PauseInput = true;
+        pauseAction.canceled += context => PauseInput = false;
+
+        jumpAction.performed += context => JumpInput = true;
+        jumpAction.canceled += context => JumpInput = false;
+
+        fireAction.performed += context => FireInput = true;
+        fireAction.canceled += context => FireInput = false;
+
+        altFireAction.performed += context => altFireInput = true;
+        altFireAction.canceled += context => altFireInput = false;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        sprintAction.Enable();
+        pauseAction.Enable();
+        jumpAction.Enable();
+        fireAction.Enable();
+        altFireAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        sprintAction.Disable();
+        pauseAction.Disable();
+        jumpAction.Disable();   
+        fireAction.Disable();
+        altFireAction.Disable();
+    }
+
+}
---
+++ Scripts/CharacterController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float SprintMultiplier = 2.0f;
+
+    private InputHandler inputHandler;
+
+    [SerializeField] private Transform orientation;
+
+    private Rigidbody rb;
+
+    private Vector3 movementDirection;
+    [SerializeField] private float jumpHeight = 10;
+
+    private Pause pauseManager;
+
+    private bool touchingGround = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pauseManager = GameObject.FindObjectOfType<Pause>();
+        inputHandler = GameObject.FindObjectOfType<InputHandler>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = orientation.forward * inputHandler.MoveInput.y + orientation.right * inputHandler.MoveInput.x;
+
+        if (inputHandler.PauseInput)
+        {
+            inputHandler.PauseInput = false;
+            pauseManager.TogglePause();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        if (inputHandler.JumpInput && touchingGround)
+        {
+            inputHandler.JumpInput = false;
+            touchingGround = false;
+            Jump();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        touchingGround = true;
+    }
+
+    void Jump()
+    {
+        rb.velocity += new Vector3(rb.position.x, rb.position.y+jumpHeight, rb.position.z);
+    }
+
+    void HandleMovement()
+    {
+        float speed = walkSpeed * (inputHandler.SprintValue > 0 ? SprintMultiplier : 1f);
+        rb.velocity = new Vector3(movementDirection.x * speed, rb.velocity.y, movementDirection.z * speed);
+    }
+}
