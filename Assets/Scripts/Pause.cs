e336ccef041f3ae43257f7d294a65759f3b0f1db
JaimeA47 <149345697+JaimeA47@users.noreply.github.com>
2025-02-28 23:44:26 -0500

    Main Commit
    commit

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool isPaused;
+
+    [SerializeField] private Canvas PauseBody;
+
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0.0f : 1.0f;
+        AudioListener.pause = isPaused;
+        PauseBody.gameObject.SetActive(isPaused);
+
+        if (!isPaused)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
